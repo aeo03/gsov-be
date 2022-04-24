7e2064c7e632ba2c299103e966ce77b120d68e62
Franz <franzedwarddayrit@gmail.com>
2022-04-25 06:44:46 +0800

    Added auth and login

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,6 +2,7 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -15,6 +16,27 @@ userRouter.post(
         });
         const createdUser = await user.save();
         res.send({ message: 'User Created', user: createdUser });
+        console.log('User ' + createdUser._id + ' Created at ' + new Date().toISOString())
+    }),
+);
+
+userRouter.post(
+    '/login',
+    expressAsyncHandler(async (req, res) => {
+        const user = await User.findOne({ name: req.body.name });
+        if (user) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                res.send({
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    token: generateToken(user),
+                });
+                console.log('User ' + user._id + ' logged in at ' + new Date().toISOString())
+                return;
+            }
+        }
+        res.status(401).send({ message: 'Invalid email or password' });
     }),
 );
 
